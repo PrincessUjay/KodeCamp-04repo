8d7a66c34b686e71948ac8b7364a9c35791750a9
PrincessUjay <onyekabaprincess@gmail.com>
2024-07-24 00:55:33 +0100

    task6

--- Assignments/Assignments/Promotional_Task6/Terraform/modules/ec2_instance/main.tf
+++ Assignments/Assignments/Promotional_Task6/Terraform/modules/ec2_instance/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "public_instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.public_subnet_id
-  security_group         = [var.public_sg_id]
+  security_groups         = [var.public_sg_id]
   associate_public_ip_address = true
   key_name                    = data.aws_key_pair.key_pair.key_name
 
@@ -24,7 +24,7 @@ resource "aws_instance" "private_instance" {
   ami               = var.ami
   instance_type     = var.instance_type
   subnet_id         = var.private_subnet_id
-  security_group    = [var.private_sg_id]
+  security_groups    = [var.private_sg_id]
   key_name                    = data.aws_key_pair.key_pair.key_name
 
   user_data = file("${path.module}/scripts/scripts/install_postgresql.sh")
