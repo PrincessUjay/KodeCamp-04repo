1bb7c83a247d8475e71c5d0510026ef82249f4ac
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-07-25 14:07:57 +0100

    Update main.tf

--- Assignments/Assignments/Promotional_Task6/Terraform/modules/route_tables/main.tf
+++ Assignments/Assignments/Promotional_Task6/Terraform/modules/route_tables/main.tf
@@ -1,23 +1,28 @@
-# modules/route_tables/main.tf
-resource "aws_route_table" "main" {
-  vpc_id = var.vpc_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = var.is_public ? var.igw_id : var.nat_gateway_id
-  }
-
-  tags = {
-    Name = var.is_public ? "PublicRouteTable" : "PrivateRouteTable"
-  }
-}
-
-resource "aws_route_table_association" "main" {
-  count = length(var.subnet_ids)
-  subnet_id      = element(var.subnet_ids, count.index)
-  route_table_id = aws_route_table.main.id
-}
-
-output "route_table_id" {
-  value = aws_route_table.main.id
-}
+# modules/route_table/main.tf
+resource "aws_route_table" "public" {
+      vpc_id = var.vpc_id
+      route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = var.igw_id
+      }
+      tags = {
+        Name = "PublicRouteTable"
+      }
+    }
+    
+    resource "aws_route_table_association" "public" {
+      subnet_id      = var.public_subnet_id
+      route_table_id = aws_route_table.public.id
+    }
+    
+    resource "aws_route_table" "private" {
+      vpc_id = var.vpc_id
+      tags = {
+        Name = "PrivateRouteTable"
+      }
+    }
+    
+    resource "aws_route_table_association" "private" {
+      subnet_id      = var.private_subnet_id
+      route_table_id = aws_route_table.private.id
+    }
