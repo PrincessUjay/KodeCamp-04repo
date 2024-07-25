0fc51ef0fd46efeace741c165d4bf21cbe47caa7
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-07-25 14:13:13 +0100

    Update variables.tf

--- Assignments/Assignments/Promotional_Task6/Terraform/modules/nat_gateway/variables.tf
+++ Assignments/Assignments/Promotional_Task6/Terraform/modules/nat_gateway/variables.tf
@@ -1,9 +1,9 @@
-   variable "public_subnet_id" {
-      description = "The public subnet ID"
-      type        = string
-    }
-    
-    variable "private_route_table_id" {
-      description = "The private route table ID"
-      type        = string
-    }
+variable "public_subnet_id" {
+  description = "ID of the public subnet where the NAT gateway will be created"
+  type        = string
+}
+
+variable "private_route_table_id" {
+  description = "ID of the private route table to associate with the NAT gateway"
+  type        = string
+}
