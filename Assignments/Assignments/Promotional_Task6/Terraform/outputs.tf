3012b8201962e6576519312295c41f76dde619de
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-07-25 13:44:54 +0100

    Update outputs.tf

--- Assignments/Assignments/Promotional_Task6/Terraform/outputs.tf
+++ Assignments/Assignments/Promotional_Task6/Terraform/outputs.tf
@@ -3,33 +3,17 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
+  value = module.subnet.public_subnet_id
 }
 
 output "private_subnet_id" {
-  value = module.vpc.private_subnet_id
-}
-
-output "igw_id" {
-  value = module.vpc.igw_id
-}
-
-output "nat_gateway_id" {
-  value = module.nat_gateway.nat_gateway_id
-}
-
-output "public_sg_id" {
-  value = module.public_sg.security_group_id
-}
-
-output "private_sg_id" {
-  value = module.private_sg.security_group_id
+  value = module.subnet.private_subnet_id
 }
 
 output "public_instance_id" {
-  value = module.public_instance.instance_id
+  value = module.ec2_instance.public_instance_id
 }
 
 output "private_instance_id" {
-  value = module.private_instance.instance_id
+  value = module.ec2_instance.private_instance_id
 }
