bdca056a1f50cc793a42246eadbd97ce90570b78
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-04 23:38:35 +0100

    Update app.py

--- Assignments/Assignments/Promotional_Task7/app.py
+++ Assignments/Assignments/Promotional_Task7/app.py
@@ -1,12 +1,17 @@
-import os
-from flask import Flask
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, Welcome to KodeCamp DevOps Bootcamp!")
 
-@app.route('/')
-def home():
-    message = os.getenv('MESSAGE', 'Hello, Welcome to KodeCamp DevOps Bootcamp!')
-    return message
+def run(server_class=HTTPServer, handler_class=MyHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print("Starting httpd server on port 8000...")
+    httpd.serve_forever()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    run()
