f1f30a728e3c9c0f855623009547774a86283a9f
Ujunwa Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2024-08-06 05:21:28 +0100

    Update app.py

--- Assignments/Assignments/Promotional_Task7/app.py
+++ Assignments/Assignments/Promotional_Task7/app.py
@@ -1,20 +1,20 @@
-  import os
-  from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-  class MyHandler(SimpleHTTPRequestHandler):
-      def do_GET(self):
-          welcome_message = os.getenv('WELCOME_MESSAGE', "Hello, Welcome to KodeCamp DevOps Bootcamp!")
-          secret_password = os.getenv('SECRET_PASSWORD', "No Secret")
-          self.send_response(200)
-          self.send_header("Content-type", "text/html")
-          self.end_headers()
-          self.wfile.write(f"{welcome_message}. Secret Password: {secret_password}".encode())
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        welcome_message = os.getenv('WELCOME_MESSAGE', "Hello, Welcome to KodeCamp DevOps Bootcamp!")
+        secret_password = os.getenv('SECRET_PASSWORD', "No Secret")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(f"{welcome_message}. Secret Password: {secret_password}".encode())
 
-  def run(server_class=HTTPServer, handler_class=MyHandler):
-      server_address = ('', 8000)
-      httpd = server_class(server_address, handler_class)
-      print("Starting httpd server on port 8000...")
-      httpd.serve_forever()
+def run(server_class=HTTPServer, handler_class=MyHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print("Starting httpd server on port 8000...")
+    httpd.serve_forever()
 
-  if __name__ == "__main__":
+if __name__ == "__main__":
       run()
